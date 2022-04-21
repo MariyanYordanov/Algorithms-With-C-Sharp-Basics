ebf2d20a08ca069cfaf9c2636c63a20f4aca9943
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-21 18:29:28 +0300

    Add Belong to array task

---
+++ Determine TheComplexityOfTheAlgorithm/BelongToArray/BelongToArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace BelongToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integerElements = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int elementToFind = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < integerElements.Length; i++)
+            {
+                if (integerElements[i] == elementToFind)
+                {
+                    Console.WriteLine($"{ elementToFind } Exists in the List");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{elementToFind} Not exists in the List");
+        }
+    }
+}
