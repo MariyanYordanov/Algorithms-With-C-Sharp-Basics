a6df4dc5076da8ab2620eefef306cd328c6615e4
Mariyan Yordanov <marsrewq@gmail.com>
2022-04-21 23:59:02 +0300

    Add

---
+++ Determine TheComplexityOfTheAlgorithm/SearchInOrderArray/SearchInOrderArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchInOrderArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] integerElements = global::System.Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int number = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < integerElements.Length; i++)
+            {
+                if (number == integerElements[i])
+                {
+                    Console.WriteLine($"{number} Exists in the List");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{number} Not exists in the List");
+        }
+    }
+}
