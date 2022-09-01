99f67827277a3c7042a584931149971364ecdbe4
Mariyan Yordanov <marsrewq@gmail.com>
2022-09-01 19:54:26 +0300

    Add Euclid's Algorithm

---
+++ Euclid's Algorithm/Euclid's Algorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿// Let's find the greatest common denominator(GCD) of two integers
+
+int greaterNumber = int.Parse(Console.ReadLine());
+int smallerNumber = int.Parse(Console.ReadLine());
+
+int result = GCD(greaterNumber, smallerNumber);
+
+Console.WriteLine(result);
+
+static int GCD(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = a;
+        a = b;
+        b = temp % b;
+    }
+
+    return a;
+}
